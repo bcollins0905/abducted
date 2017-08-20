41fd87f6f01be4b3a2477ae24b9c300049f24ff9
bcollins0905 <b.w.collinssf@gmail.com>
2017-08-20 15:32:12 -0700

    Beginning Lines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,21 @@
-alert("You are about to enter a site.")
+
+var words = ['centaurus', 'martian', 'abduction', 'specimen', 'crop circles', 'little green men', 'beam me up scotty'];
+
+// This function will pick our word
+var randWord = words[Math.floor(Math.random() * words.length)];
+// This gives us the blanks
+
+var answerArray = [];
+for (var i = 0; i < randWord.length; i++) {
+ answerArray[i] = "_";
+}
+
+document.getElementById('answerArray').innerHTML
+// This determines how many tries user gets 
+var wrongGuess = [];
+for (var j = 0; j < wrongGuess.length; j++ > 9){
+    
+}
+
+
+
