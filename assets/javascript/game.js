6b628233dcefb5fe600d11f78a25d78bd7581833
Brad <b.w.collinssf@gmail.com>
2017-08-21 00:02:57 -0700

    Revert "Some pointers"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,12 @@ var words = ['centaurus', 'martian', 'abduction', 'specimen', 'crop circles', 'l
 var randWord = words[Math.floor(Math.random() * words.length)];
 // This gives us the blanks
 
-// I'd suggest adding console.logs all over the place so you can verify your assumptions by checking the console
-// It's also a nice way to see intermediate process when nothing's appearing on the screen quite yet
-console.log('randWord: ', randWord)
-
 var answerArray = [];
 for (var i = 0; i < randWord.length; i++) {
  answerArray[i] = "_";
 }
 
-// By passing in `answerArray` to getElementById here, you are telling
-// the browser to try and find an element in your html with an id of `answerArray`
-// while you have an answerArray variable in this script, you don't have
-// an element with that id in your index.html
-var answerArrayElement = document.getElementById('answerArray')
-console.log('answerArrayElement: ', answerArrayElement)
-
-// I added a div with a gameboard id, so this call to getElementById
-// will return that element. Open this up in the console to see what I'm talking about!
-var gameboardElement = document.getElementById('gameboard')
-console.log('gameboardElement: ', gameboardElement)
-
-
+document.getElementById('answerArray').innerHTML
 // This determines how many tries user gets 
 var wrongGuess = [];
 for (var j = 0; j < wrongGuess.length; j++ > 9){
